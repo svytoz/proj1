44d6b771f1222fb47fcd2d0ba87a0c1c089d2ab7
svytoz <Virusaka@me.com>
2025-09-03 23:10:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const button = document.querySelector('button');
 const output = document.querySelector ('.output');
 
 button.addEventListener('click', function() {
-    output.innerHTML = 'Всем привет!';
+    output.innerHTML = 'Коты близко!';
 });
