3b36298115de4206cc9af07439f483ce1c8f4e80
svytoz <Virusaka@me.com>
2025-09-06 16:23:32 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('button');
-const output = document.querySelector ('.output');
-
-button.addEventListener('click', function() {
-    output.innerHTML = 'Коты близко!';
-});
